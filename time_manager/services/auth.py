08b9b729e74df0b0f5d1ed047203ac39fb60a26d
Lawrence Naumov <prostolawr@gmail.com>
2023-08-09 05:11:35 +0800

    Add session lifetime

--- time_manager/services/auth.py
+++ time_manager/services/auth.py
@@ -19,7 +19,7 @@ ALGORITHM = "HS256"
 
 class AuthService(BaseService):
     SECRET_KEY = settings.SECRET_KEY
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES = settings.SESSION_LIFETIME
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     oauth2_scheme = OAuth2PasswordBearer(
         tokenUrl="/api/login",
